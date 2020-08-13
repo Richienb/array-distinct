b18a16b6aa3a889efa548eae60b8e8c792bf85ec
Richie Bendall <richiebendall@gmail.com>
2020-08-14 04:35:46 +1200

    Increase performance
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ module.exports = array => {
 		throw new TypeError(`Expected an array, got ${typeof array}`)
 	}
 
-	return array.filter(value => array.indexOf(value) === array.lastIndexOf(value))
+	const result = new Set()
+	const seenItems = new Set()
+	for (const item of array) {
+		if (seenItems.has(item)) {
+			continue
+		} else if (result.has(item)) {
+			result.delete(item)
+			seenItems.add(item)
+		} else {
+			result.add(item)
+		}
+	}
+
+	return [...result]
 }
